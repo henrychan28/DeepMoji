5f37043cf69c45eadd1035e942b3da950b5ec6ec
henrychan28 <henrychan280@gmail.com>
2018-04-23 19:57:29 +0800

    test

---
+++ examples/playgound.py
@@ -0,0 +1,18 @@
+from __future__ import print_function
+import example_helper
+import json
+from deepmoji.model_def import deepmoji_transfer
+from deepmoji.global_variables import PRETRAINED_PATH
+from deepmoji.finetuning import (
+    load_benchmark,
+    finetune)
+
+DATASET_PATH = '../data/SS-Youtube/raw.pickle'
+nb_classes = 2
+
+with open('../model/vocabulary.json', 'r') as f:
+    vocab = json.load(f)
+
+# Load dataset.
+data = load_benchmark(DATASET_PATH, vocab)
+print(data)
